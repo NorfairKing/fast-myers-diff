714007f4d677b8dc5d345eac076ad723e6f8b3a7
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-08-12 08:31:35 +0200

    fix the readme link

--- fast-myers-diff/default.nix
+++ fast-myers-diff/default.nix
@@ -5,7 +5,7 @@ mkDerivation {
   src = ./.;
   libraryHaskellDepends = [ base dlist text vector ];
   testHaskellDepends = [ base hspec text vector ];
-  homepage = "https://github.com/NorfairKing/sydtest#readme";
+  homepage = "https://github.com/NorfairKing/fast-myers-diff#readme";
   description = "A fast implementation of the Myers diff algorithm";
   license = lib.licenses.mit;
 }
