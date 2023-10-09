c106ef803c69ef9440399247ec4ffd59c84f6fc7
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-10-09 22:58:40 +0200

    forward compatibility

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-23.05";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
-    horizon-core.url = "git+https://gitlab.horizon-haskell.net/package-sets/horizon-core";
+    horizon-advance.url = "git+https://gitlab.horizon-haskell.net/package-sets/horizon-advance";
     nixpkgs-22_11.url = "github:NixOS/nixpkgs?ref=nixos-22.11";
     nixpkgs-22_05.url = "github:NixOS/nixpkgs?ref=nixos-22.05";
     nixpkgs-21_11.url = "github:NixOS/nixpkgs?ref=nixos-21.11";
@@ -15,41 +15,27 @@
     , nixpkgs-22_11
     , nixpkgs-22_05
     , nixpkgs-21_11
+    , horizon-advance
     , pre-commit-hooks
-    , horizon-core
     }:
     let
       system = "x86_64-linux";
-      overlays = [
-        self.overlays.${system}
+      nixpkgsFor = nixpkgs: import nixpkgs { inherit system; config.allowUnfree = true; };
+      pkgs = nixpkgsFor nixpkgs;
+      allOverrides = pkgs.lib.composeManyExtensions [
+        self.overrides.${system}
       ];
-      pkgsFor = nixpkgs: import nixpkgs {
-        inherit system;
-        inherit overlays;
-      };
-      horizonPkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          (final: prev: {
-            haskellPackages = prev.haskellPackages.override (old: {
-              overrides = final.lib.composeExtensions (old.overrides or (_: _: { })) (self: super:
-                horizon-core.legacyPackages.${system}
-              );
-            });
-          })
-        ] ++ overlays;
-      };
-
-      pkgs = pkgsFor nixpkgs;
+      horizonPkgs = horizon-advance.legacyPackages.${system}.extend allOverrides;
+      haskellPackagesFor = nixpkgs: (nixpkgsFor nixpkgs).haskellPackages.extend allOverrides;
+      haskellPackages = haskellPackagesFor nixpkgs;
     in
     {
+      overrides.${system} = pkgs.callPackage ./nix/overrides.nix { };
       overlays.${system} = import ./nix/overlay.nix;
-      packages.${system}.default = pkgs.haskellPackages.fast-myers-diff;
+      packages.${system}.default = haskellPackages.fast-myers-diff;
       checks.${system} =
         let
-          backwardCompatibilityCheckFor = nixpkgs:
-            let pkgs' = pkgsFor nixpkgs;
-            in pkgs'.haskellPackages.fast-myers-diff;
+          backwardCompatibilityCheckFor = nixpkgs: (haskellPackagesFor nixpkgs).fast-myers-diff;
           allNixpkgs = {
             inherit
               nixpkgs-22_11
@@ -59,7 +45,7 @@
           backwardCompatibilityChecks = pkgs.lib.mapAttrs (_: nixpkgs: backwardCompatibilityCheckFor nixpkgs) allNixpkgs;
         in
         backwardCompatibilityChecks // {
-          forwardCompatibility = horizonPkgs.haskellPackages.fast-myers-diff;
+          forwardCompatibility = horizonPkgs.fast-myers-diff;
           release = self.packages.${system}.default;
           shell = self.devShells.${system}.default;
           pre-commit = pre-commit-hooks.lib.${system}.run {
@@ -74,7 +60,7 @@
             };
           };
         };
-      devShells.${system}.default = pkgs.haskellPackages.shellFor {
+      devShells.${system}.default = haskellPackages.shellFor {
         name = "fast-myers-diff-shell";
         packages = p: [ p.fast-myers-diff ];
         withHoogle = true;
